59371c41232864f46d56654723d5a0de9c9f9ef4
dylanfernandes <dylangfernandes@gmail.com>
2019-01-28 21:25:12 -0500

    Added tests for input file 1

---
+++ src/test/java/lexer/inputFile/InputFile1.java
@@ -0,0 +1,69 @@
+package lexer.inputFile;
+
+import lexer.LexerDriver;
+import lexer.Token;
+import lexer.Tokenizer;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+
+public class InputFile1 {
+
+    @Test
+    public void testTokenRetriveal() throws FileNotFoundException {
+        LexerDriver lexerDriver = new LexerDriver("src/input/examples/input1.txt");
+        String input = lexerDriver.getInput();
+        Tokenizer tokenizer = new Tokenizer(input);
+
+        Token token = tokenizer.nextToken();
+        assertEquals(Token.TokenType.INTEGER, token.getType());
+        assertEquals("integer", token.getLexeme());
+
+        token = tokenizer.nextToken();
+        assertEquals(Token.TokenType.MAIN, token.getType());
+        assertEquals("main", token.getLexeme());
+
+        token = tokenizer.nextToken();
+        assertEquals(Token.TokenType.OPAR, token.getType());
+        assertEquals("(", token.getLexeme());
+
+        token = tokenizer.nextToken();
+        assertEquals(Token.TokenType.CPAR, token.getType());
+        assertEquals(")", token.getLexeme());
+
+        token = tokenizer.nextToken();
+        assertEquals(Token.TokenType.OBRA, token.getType());
+        assertEquals("{", token.getLexeme());
+
+
+        token = tokenizer.nextToken();
+        assertEquals(Token.TokenType.NEWLINE, token.getType());
+        assertEquals("\n", token.getLexeme());
+
+        token = tokenizer.nextToken();
+        assertEquals(Token.TokenType.CMT, token.getType());
+        assertEquals("//comment", token.getLexeme());
+
+        token = tokenizer.nextToken();
+        assertEquals(Token.TokenType.ID, token.getType());
+        assertEquals("a", token.getLexeme());
+
+        token = tokenizer.nextToken();
+        assertEquals(Token.TokenType.ASSGN, token.getType());
+        assertEquals("=", token.getLexeme());
+
+        token = tokenizer.nextToken();
+        assertEquals(Token.TokenType.FLO, token.getType());
+        assertEquals("2.3e-10", token.getLexeme());
+
+        token = tokenizer.nextToken();
+        assertEquals(Token.TokenType.NEWLINE, token.getType());
+        assertEquals("\n", token.getLexeme());
+
+        token = tokenizer.nextToken();
+        assertEquals(Token.TokenType.CBRA, token.getType());
+        assertEquals("}", token.getLexeme());
+    }
+}
