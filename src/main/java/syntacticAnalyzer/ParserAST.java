e3c08c5237e19bc0f43394c68389322278a73aec
dylanfernandes <dylangfernandes@gmail.com>
2019-03-10 17:00:33 -0400

    Start of AST integration in parser
    Signed-off-by: dylanfernandes <dylangfernandes@gmail.com>

---
+++ src/main/java/syntacticAnalyzer/ParserAST.java
@@ -0,0 +1,156 @@
+package syntacticAnalyzer;
+
+import lexer.Token;
+import syntacticAnalyzer.AST.ASTNode;
+import syntacticAnalyzer.AST.ASTNodeFactory;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ParserAST {
+
+    private List<Token> tokenList;
+    private int position;
+    private Token lookahead;
+    private String syntax;
+    private boolean parseGood;
+    private boolean foundError;
+    private ASTNode root;
+
+    public ParserAST() {
+        position = 0;
+        syntax = "";
+        parseGood = false;
+        foundError = false;
+        root = null;
+    }
+
+    public ParserAST(List<Token> tokens) {
+        new Parser();
+        tokenList = tokens;
+        if(tokenList.size() > 0) {
+            lookahead = tokenList.get(0);
+        }
+    }
+
+    public String getSyntax() {
+        return syntax;
+    }
+
+    public void setTokenList(List<Token> tokenList) {
+        this.tokenList = tokenList;
+        lookahead = tokenList.get(0);
+    }
+
+    public boolean isParseGood() {
+        return parseGood;
+    }
+
+    public boolean isFoundError() {
+        return foundError;
+    }
+
+    private boolean hasNextToken() {
+        return position + 1 < tokenList.size();
+    }
+
+    public Token nextToken() {
+        Token token;
+        if(hasNextToken()) {
+            position++;
+            token = tokenList.get(position);
+            return  token;
+        }
+        return  null;
+    }
+
+    public boolean skipErrors(List<Token.TokenType> first, List<Token.TokenType> follow, boolean write) {
+        if(lookahead != null) {
+            if (first.contains(lookahead.getType()) || (first.contains(Token.TokenType.EPSILON) && follow.contains(lookahead.getType()))) {
+                return true;
+            } else {
+                if(write) {
+                    addToSyntax("Syntax error at line: " + lookahead.getLineNumber());
+                    foundError = true;
+                    //if in follow, current production not good, can get epsilon if in first in parse method
+                    while (!first.contains(lookahead.getType()) && !follow.contains(lookahead.getType())) {
+                        lookahead = nextToken();
+                        if (lookahead == null)
+                            break;
+                        //production not properly completed
+                        if (first.contains(Token.TokenType.EPSILON) && follow.contains(lookahead.getType()))
+                            return false;
+                    }
+                    if (lookahead != null)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    //write error in syntax automatically
+    public boolean skipErrors(List<Token.TokenType> first, List<Token.TokenType> follow) {
+        return skipErrors(first, follow, true);
+    }
+
+
+    public boolean match( Token.TokenType expectedTokenType) {
+        if(lookahead != null && lookahead.getType() == expectedTokenType) {
+            lookahead = nextToken();
+            return true;
+        }
+        return  false;
+    }
+
+    public boolean matchAndSave( Token.TokenType expectedTokenType, Token storageToken) {
+        if(lookahead != null && lookahead.getType() == expectedTokenType) {
+            storageToken.setToken(lookahead);
+            lookahead = nextToken();
+            return true;
+        }
+        return  false;
+    }
+
+    public boolean peekListMatch(List<Token.TokenType> tokenTypes) {
+        return lookahead != null && tokenTypes.contains(lookahead.getType());
+    }
+
+    public boolean peekMatch(Token.TokenType expectedTokenType) {
+        return lookahead != null && lookahead.getType() == expectedTokenType;
+    }
+
+    public void addToSyntax(String newSyntax) {
+        syntax = newSyntax + "\n" + syntax;
+    }
+
+    public String parse() {
+        parseGood = prog();
+        return syntax;
+    }
+
+    /******************************************************************
+     Start of grammar production implementation
+     *******************************************************************/
+
+    public boolean prog() {
+        root = ASTNodeFactory.getASTNode("prog");
+        ASTNode classDeclRepNode = ASTNodeFactory.getASTNode("classDeclRep");
+        ASTNode funcDefRepNode = ASTNodeFactory.getASTNode("funcDefRep");
+        ASTNode funcBodyNode = ASTNodeFactory.getASTNode("funcBody");
+        Token main = new Token();
+        Token semi = new Token();
+
+        if(!skipErrors(Arrays.asList(Token.TokenType.INTEGER, Token.TokenType.FLOAT, Token.TokenType.CLASS, Token.TokenType.ID, Token.TokenType.MAIN), Collections.<Token.TokenType>emptyList())) {
+            return false;
+        }
+
+//        if (classDeclRep(classDeclRepNode) && funcDefRep(funcDefRepNode) && matchAndSave(Token.TokenType.MAIN, main) && funcBody(funcBodyNode) && matchAndSave(Token.TokenType.SEMI, semi)) {
+//            addToSyntax("prog -> classDeclRep funcDefRep 'main' funcBody ';'");
+//            root.makeFamily(classDeclRepNode, ASTNodeFactory.getASTNode(main),funcDefRepNode, funcBodyNode, ASTNodeFactory.getASTNode(semi));
+//            return true;
+//        }
+            return false;
+    }
+}
