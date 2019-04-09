a68a5cf5c3d8eb039ca53adbb407c568a1fa6915
Dylan Fernandes <dylangfernandes@gmail.com>
2019-04-09 12:59:12 -0400

    Removed putint dependency from template
    Signed-off-by: Dylan Fernandes <dylangfernandes@gmail.com>

--- src/moon/template.m
+++ src/moon/template.m
@@ -5,6 +5,7 @@ entry
 allign
       addi r3,r0, 7
       sw x(r2), r3
+      addi r3, r0, 8
       sw y(r2), r3
 
 % Perform each arithmetic operation with X and Y
@@ -13,6 +14,33 @@ allign
       %sb     tab(r1),r2
       %addi   r1,r0,0
 
-lw r1, x(r2)
+lw r1, y(r2)
 jl r15,putint
 hlt
+
+putint   add    r2,r0,r0         % c := 0 (character)
+         add    r3,r0,r0         % s := 0 (sign)
+         addi   r4,r0,endbuf     % p is the buffer pointer
+         cge    r5,r1,r0
+         bnz    r5,putint1       % branch if n >= 0
+         addi   r3,r0,1          % s := 1
+         sub    r1,r0,r1         % n := -n
+putint1  modi   r2,r1,10         % c := n mod 10
+         addi   r2,r2,48         % c := c + '0'
+         subi   r4,r4,1          % p := p - 1
+         sb     0(r4),r2         % buf[p] := c
+         divi   r1,r1,10         % n := n div 10
+         bnz    r1,putint1       % do next digit
+         bz     r3,putint2       % branch if n >= 0
+         addi   r2,r0,45         % c := '-'
+         subi   r4,r4,1          % p := p - 1
+         sb     0(r4),r2         % buf[p] := c
+putint2  lb     r2,0(r4)         % c := buf[p]
+         putc   r2               % write c
+         addi   r4,r4,1          % p := p + 1
+         cgei   r5,r4,endbuf
+         bz     r5,putint2       % branch if more digits
+         jr     r15              % return
+
+         res    20               % digit buffer
+endbuf
